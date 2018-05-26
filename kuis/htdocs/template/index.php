6a3e9c9ea32e1d59fb3e3d46277351dd2ea47ab1
Gunawan5445 <gunawan.0277@students.amikom.ac.id>
2018-05-26 12:00:01 +0700

    publish kuis

---
+++ kuis/htdocs/template/index.php
@@ -0,0 +1,96 @@
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Membuat Template Website Bootstrap</title>
+
+    <!-- Bootstrap -->
+    <link href="assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of 
+         HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+    <!-- Custom CSS -->
+    <link href="assets/style.css" rel="stylesheet">
+
+  </head>
+  <body>
+    <div class="container">
+
+    <!-- Bagian Header -->
+    <div class="row">
+        <div class="col-md-12 header" id="site-header">
+            <header>
+    <h1 class="title-site">Gunawan</h1>
+</header>
+<nav class="menus">
+    <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About Us</a></li>
+        <li><a href="#">Portfolio</a></li>
+    </ul>
+</nav>
+        </div>
+    </div>
+    <!-- End Bagian Header -->
+
+    <!-- Bagian Wrapper 2 kolom -->
+    <div class="row">
+        <div class="col-md-8 articles" id="site-content">
+           <article class="posts">
+    <h2 class="title-post">Membaca dan selalu awali dengan bissmillah</h2>
+    <div class="meta-post">
+        <span><em class="glyphicon glyphicon-user"></em> Yogyakarta,</span>
+        <span><em class="glyphicon glyphicon-time"></em> 19 mei 2018</span>
+    </div>
+    <div class="content">
+        <p>Selamat datang , semoga anda dapat memahami dengan sesama dan membaca dengan cara yang sederhana</p>
+        <p>Mari membudayakan membaca,jangan hanya diam tanpa mengetahui berita</p>
+        
+    </div>
+</article>
+        </div>
+        <div class="col-md-4 sidebar" id="site-sidebar">
+            <aside class="widgets">
+    <h3 class="widget-title">Latest Posts</h3>
+    <ul>
+        <li><a href="#">Manfaat Membaca</a></li>
+        <li><a href="#">Cara Cepat Tidur</a></li>
+        <li><a href="#">Keutamaan tidur</a></li>
+        <li><a href="#">Motivasi Hidup</a></li>
+    </ul>
+</aside>
+        </div>
+    </div>
+    <!-- End Bagian wrapper -->
+    
+    <footer class=”copyright text-center”><p>&copy; 2018 gunawan.com</p></footer>
+    <div class="row">
+        <div class="col-md-12 footer" id="site-footer">
+            <!-- isi footer -->
+        </div>
+    </div>
+    <!-- End Bagian footer -->
+
+</div>
+    <!--happy code-->
+
+
+    <!-- jQuery online menggunakan CDN -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    
+    <!-- jQuery lokal -->
+    <script src="assets/jquery/jquery.min.js"></script>
+ 
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    
+    <!-- Custom JS -->
+    <script src="assets/custom.js"></script>
+  </body>
+</html>
